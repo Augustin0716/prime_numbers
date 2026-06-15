55592da184d805c16ad284c0375bbf6a06a80756
Augustin0716 <augustin.sathicq@gmail.com>
2026-06-15 18:20:36 +0200

    Modified the test so it doesn't cover for bugs anymore

--- tests/save_test.py
+++ tests/save_test.py
@@ -2,10 +2,9 @@ from src.prime_numbers import prime_generator, __primes, __unordered_primes # do
 
 print(__primes)
 print(__unordered_primes)
-g = prime_generator()
+g = prime_generator(100)
 for p in g:
-    if p > 500:
-        break
+    pass
 
-print("Final List :", __primes)
+print("Final List :", __primes, f"\n{len(__primes)} elements")
 print("Final set :", __unordered_primes)
