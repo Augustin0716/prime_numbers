72c0b963ae2234be64bacb6e582764fdfc6df9df
Augustin0716 <augustin.sathicq@gmail.com>
2026-06-16 18:47:24 +0200

    Made a 'if __name__ == '__main__'' block because else it won't work properly

--- tests/save_test.py
+++ tests/save_test.py
@@ -1,10 +1,11 @@
 from src.prime_numbers import prime_generator, __primes, __unordered_primes # don't do that kids
 
-print(__primes)
-print(__unordered_primes)
-g = prime_generator(100)
-for p in g:
-    pass
+if __name__ == "__main__":
+    print(__primes)
+    print(__unordered_primes)
+    g = prime_generator(100)
+    for p in g:
+        pass
 
-print("Final List :", __primes, f"\n{len(__primes)} elements")
-print("Final set :", __unordered_primes)
+    print("Final List :", __primes, f"\n{len(__primes)} elements")
+    print("Final set :", __unordered_primes)
